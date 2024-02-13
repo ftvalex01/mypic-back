4702752edc0560dee75c64fb6144b5fcbf65150f
LucasVega2004 <luquitasvegablanco@gmail.com>
2024-02-13 19:04:25 +0000

    notificaiones

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -9,16 +9,20 @@ use App\Http\Resources\NotificationResource;
 use App\Models\Notification;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
 
 class NotificationController extends Controller
 {
-    public function index(Request $request): Response
+    public function index(Request $request)
     {
-        $notifications = Notification::all();
+        $user = Auth::user();
+        Log::info("User ID: " . $user->id); // Asegúrate de importar la fachada Log
 
-        return new NotificationCollection($notifications);
+        $notifications = $user->notifications()->get();
+
+        return response()->json($notifications);
     }
-
     public function store(NotificationStoreRequest $request): Response
     {
         $notification = Notification::create($request->validated());
