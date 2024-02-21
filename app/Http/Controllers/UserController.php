184856ef58ab49eda8888383281beac5c20d94af
LucasVega2004 <luquitasvegablanco@gmail.com>
2024-02-21 18:12:01 +0000

    usuario bloqueado implementado

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -51,7 +51,7 @@ class UserController extends Controller
     }
 
     public function checkUsernameAvailability($username)
-    {   
+    {
         Log::info($username);
         $userExists = User::where('username', $username)->exists();
         Log::info($userExists);
@@ -60,18 +60,18 @@ class UserController extends Controller
         }
         return response()->json(['message' => 'Username is available'], Response::HTTP_OK);
     }
-    
+
     // Método para cargar el perfil de usuario
     public function getUserByUsername($username)
     {
         $user = User::where('username', $username)->first();
-    
+
         if (!$user) {
             return response()->json(['message' => 'User not found'], Response::HTTP_NOT_FOUND);
         }
         return response()->json($user);
     }
-    
+
     public function store(Request $request)
     {
         try {
@@ -90,7 +90,7 @@ class UserController extends Controller
                 'password' => Hash::make($request->password),
                 'birth_date' => $request->birth_date,
                 'register_date' => now(),
-                'is_2fa_enabled' =>$request->enable2FA,
+                'is_2fa_enabled' => $request->enable2FA,
             ]);
 
             event(new Registered($user));
@@ -107,113 +107,113 @@ class UserController extends Controller
         }
     }
     public function toggleFollow(Request $request, $userId)
-{
-    $userToFollow = User::find($userId);
-    if (!$userToFollow) {
-        return response()->json(['error' => 'User not found'], 404);
-    }
-
-    $currentUser = auth()->user();
-
-    // Verificar si el usuario actual ya sigue al usuario objetivo
-    if ($currentUser->following()->where('user_id', $userId)->exists()) {
-        // Ya está siguiendo al usuario, procede a dejar de seguir
-        $currentUser->following()->detach($userId);
-        return response()->json(['isFollowing' => false]);
-    } else if ($userToFollow->is_private) {
-        // El perfil es privado, manejar solicitud de seguimiento
-        $existingNotification = Notification::where([
-            'user_id' => $userId,
-            'related_id' => $currentUser->id,
-            'type' => 'follow_request',
-        ])->first();
-
-        if ($existingNotification) {
-            // Ya existe una solicitud de seguimiento pendiente, proceder a cancelarla
-            $existingNotification->delete(); // Eliminar la solicitud existente
-            return response()->json(['message' => 'Follow request cancelled', 'isRequested' => false]);
-        } else {
-            // Crear una nueva solicitud de seguimiento
-            Notification::create([
-                'user_id' => $userId,
-                'type' => 'follow_request',
-                'related_id' => $currentUser->id,
-                'read' => false,
-                'notification_date' => now(),
-            ]);
-
-            return response()->json(['message' => 'Follow request sent', 'isRequested' => true]);
+    {
+        $userToFollow = User::find($userId);
+        if (!$userToFollow) {
+            return response()->json(['error' => 'User not found'], 404);
         }
-    } else {
-        // El perfil no es privado, proceder a seguir al usuario
-        $currentUser->following()->attach($userId);
-        return response()->json(['isFollowing' => true]);
-    }
-}
-    
-public function rejectFollowRequest(Request $request, $notificationId)
-{
-    $notification = Notification::find($notificationId);
 
-    if (!$notification || $notification->type !== 'follow_request') {
-        return response()->json(['message' => 'Invalid request'], 404);
-    }
+        $currentUser = auth()->user();
 
-    // Asegurarse de que el usuario autenticado es el destinatario de la solicitud de seguimiento
-    if ($notification->user_id !== auth()->id()) {
-        return response()->json(['message' => 'Unauthorized'], 403);
-    }
+        // Verificar si el usuario actual ya sigue al usuario objetivo
+        if ($currentUser->following()->where('user_id', $userId)->exists()) {
+            // Ya está siguiendo al usuario, procede a dejar de seguir
+            $currentUser->following()->detach($userId);
+            return response()->json(['isFollowing' => false]);
+        } else if ($userToFollow->is_private) {
+            // El perfil es privado, manejar solicitud de seguimiento
+            $existingNotification = Notification::where([
+                'user_id' => $userId,
+                'related_id' => $currentUser->id,
+                'type' => 'follow_request',
+            ])->first();
 
-    // Eliminar la notificación
-    $notification->delete();
+            if ($existingNotification) {
+                // Ya existe una solicitud de seguimiento pendiente, proceder a cancelarla
+                $existingNotification->delete(); // Eliminar la solicitud existente
+                return response()->json(['message' => 'Follow request cancelled', 'isRequested' => false]);
+            } else {
+                // Crear una nueva solicitud de seguimiento
+                Notification::create([
+                    'user_id' => $userId,
+                    'type' => 'follow_request',
+                    'related_id' => $currentUser->id,
+                    'read' => false,
+                    'notification_date' => now(),
+                ]);
 
-    return response()->json(['message' => 'Follow request rejected']);
-}
-
-public function acceptFollowRequest(Request $request, $notificationId)
-{
-    $notification = Notification::find($notificationId);
-
-    if (!$notification || $notification->type !== 'follow_request') {
-        return response()->json(['message' => 'Invalid request'], 404);
-    }
-
-    // Asegurarse de que el usuario autenticado es el destinatario de la solicitud de seguimiento
-    if ($notification->user_id !== auth()->id()) {
-        return response()->json(['message' => 'Unauthorized'], 403);
-    }
-
-    // Establecer la relación de seguimiento
-    $followerId = $notification->related_id;
-    $user = User::find(auth()->id());
-    $user->followers()->attach($followerId);
-
-    // Marcar la notificación como leída o eliminarla
-    $notification->delete();
-
-    return response()->json(['message' => 'Follow request accepted']);
-}
-
-public function updatePrivacy(Request $request, $userId)
-{
-    $user = auth()->user();
-
-    // Asegúrate de que el usuario solo pueda actualizar su propia privacidad
-    if ($user->id != $userId) {
-        return response()->json(['message' => 'Unauthorized'], 403);
+                return response()->json(['message' => 'Follow request sent', 'isRequested' => true]);
+            }
+        } else {
+            // El perfil no es privado, proceder a seguir al usuario
+            $currentUser->following()->attach($userId);
+            return response()->json(['isFollowing' => true]);
+        }
     }
 
-    // Validar la entrada
-    $data = $request->validate([
-        'isPrivate' => 'required|boolean',
-    ]);
+    public function rejectFollowRequest(Request $request, $notificationId)
+    {
+        $notification = Notification::find($notificationId);
 
-    // Actualizar la privacidad del perfil
-    $user->is_private = $data['isPrivate'];
-    $user->save();
+        if (!$notification || $notification->type !== 'follow_request') {
+            return response()->json(['message' => 'Invalid request'], 404);
+        }
 
-    return response()->json(['isPrivate' => $user->is_private]);
-}
+        // Asegurarse de que el usuario autenticado es el destinatario de la solicitud de seguimiento
+        if ($notification->user_id !== auth()->id()) {
+            return response()->json(['message' => 'Unauthorized'], 403);
+        }
+
+        // Eliminar la notificación
+        $notification->delete();
+
+        return response()->json(['message' => 'Follow request rejected']);
+    }
+
+    public function acceptFollowRequest(Request $request, $notificationId)
+    {
+        $notification = Notification::find($notificationId);
+
+        if (!$notification || $notification->type !== 'follow_request') {
+            return response()->json(['message' => 'Invalid request'], 404);
+        }
+
+        // Asegurarse de que el usuario autenticado es el destinatario de la solicitud de seguimiento
+        if ($notification->user_id !== auth()->id()) {
+            return response()->json(['message' => 'Unauthorized'], 403);
+        }
+
+        // Establecer la relación de seguimiento
+        $followerId = $notification->related_id;
+        $user = User::find(auth()->id());
+        $user->followers()->attach($followerId);
+
+        // Marcar la notificación como leída o eliminarla
+        $notification->delete();
+
+        return response()->json(['message' => 'Follow request accepted']);
+    }
+
+    public function updatePrivacy(Request $request, $userId)
+    {
+        $user = auth()->user();
+
+        // Asegúrate de que el usuario solo pueda actualizar su propia privacidad
+        if ($user->id != $userId) {
+            return response()->json(['message' => 'Unauthorized'], 403);
+        }
+
+        // Validar la entrada
+        $data = $request->validate([
+            'isPrivate' => 'required|boolean',
+        ]);
+
+        // Actualizar la privacidad del perfil
+        $user->is_private = $data['isPrivate'];
+        $user->save();
+
+        return response()->json(['isPrivate' => $user->is_private]);
+    }
 
 
     public function follows()
@@ -242,54 +242,53 @@ public function updatePrivacy(Request $request, $userId)
     }
 
     public function login(LoginRequest $request)
-{
-    $request->authenticate();
-    $request->session()->regenerate();
-    $user = $request->user();
+    {
+        $request->authenticate();
+        $request->session()->regenerate();
+        $user = $request->user();
 
-    if ($user->is_2fa_enabled) {
-        $token = Str::random(6); // Genera un código de 6 caracteres
+        if ($user->is_2fa_enabled) {
+            $token = Str::random(6); // Genera un código de 6 caracteres
 
-        // Enviar el token por correo electrónico al usuario
-        Mail::to($user->email)->send(new TwoFACodeMail($token));
+            // Enviar el token por correo electrónico al usuario
+            Mail::to($user->email)->send(new TwoFACodeMail($token));
 
 
-        // Guardar el token en la base de datos para su posterior verificación
-        // Asumiendo que tienes una columna `two_fa_code` y `two_fa_expires_at` en tu tabla de usuarios
-        $user->two_fa_code = Hash::make($token);
-        $user->two_fa_expires_at = now()->addMinutes(10); // El código expira en 10 minutos
+            // Guardar el token en la base de datos para su posterior verificación
+            // Asumiendo que tienes una columna `two_fa_code` y `two_fa_expires_at` en tu tabla de usuarios
+            $user->two_fa_code = Hash::make($token);
+            $user->two_fa_expires_at = now()->addMinutes(10); // El código expira en 10 minutos
+            $user->save();
+
+            return response()->json(['message' => '2FA code sent to your email.', 'requires_2fa_verification' => true]);
+        }
+
+        return new UserResource($user);
+    }
+    public function verify2FA(Request $request)
+    {
+
+        $request->validate([
+            'code' => 'required',
+            'email' => 'required|email',
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user || !Hash::check($request->code, $user->two_fa_code) || $user->two_fa_expires_at < now()) {
+            return response()->json(['message' => 'El código de verificación es incorrecto o ha expirado.'], 401);
+        }
+
+        // Código correcto, procede a limpiar el código 2FA y fecha de expiración
+        $user->two_fa_code = null;
+        $user->two_fa_expires_at = null;
         $user->save();
 
-        return response()->json(['message' => '2FA code sent to your email.', 'requires_2fa_verification' => true]);
-    }
-  
+        // Aquí, procede a realizar cualquier otra lógica necesaria post-verificación,
+        // como actualizar el estado de sesión del usuario para indicar que está completamente autenticado.
+
         return new UserResource($user);
-    
-}
-public function verify2FA(Request $request)
-{
-  
-    $request->validate([
-        'code' => 'required',
-        'email' => 'required|email',
-    ]);
-
-    $user = User::where('email', $request->email)->first();
-
-    if (!$user || !Hash::check($request->code, $user->two_fa_code) || $user->two_fa_expires_at < now()) {
-        return response()->json(['message' => 'El código de verificación es incorrecto o ha expirado.'], 401);
     }
-
-    // Código correcto, procede a limpiar el código 2FA y fecha de expiración
-    $user->two_fa_code = null;
-    $user->two_fa_expires_at = null;
-    $user->save();
-
-    // Aquí, procede a realizar cualquier otra lógica necesaria post-verificación,
-    // como actualizar el estado de sesión del usuario para indicar que está completamente autenticado.
-
-    return new UserResource($user);
-}
     /**
      * Handle an incoming password reset link request.
      *
@@ -349,26 +348,26 @@ public function verify2FA(Request $request)
     }
 
     public function followData($userId)
-{
-    $user = User::withCount(['followers', 'following'])->find($userId);
-    if (!$user) {
-        return response()->json(['message' => 'User not found'], 404);
-    }
+    {
+        $user = User::withCount(['followers', 'following'])->find($userId);
+        if (!$user) {
+            return response()->json(['message' => 'User not found'], 404);
+        }
 
-    // Obtiene el usuario actual autenticado
-    $currentUser = auth()->user();
-    // Verifica si el usuario actual está siguiendo al usuario de destino
-    $isFollowing = false;
-    if ($currentUser) {
-        $isFollowing = $currentUser->following()->where('users.id', $userId)->exists();
-    }
+        // Obtiene el usuario actual autenticado
+        $currentUser = auth()->user();
+        // Verifica si el usuario actual está siguiendo al usuario de destino
+        $isFollowing = false;
+        if ($currentUser) {
+            $isFollowing = $currentUser->following()->where('users.id', $userId)->exists();
+        }
 
-    return response()->json([
-        'followersCount' => $user->followers_count,
-        'followingCount' => $user->following_count,
-        'isFollowing' => $isFollowing,
-    ]);
-}
+        return response()->json([
+            'followersCount' => $user->followers_count,
+            'followingCount' => $user->following_count,
+            'isFollowing' => $isFollowing,
+        ]);
+    }
 
     public function profile(Request $request)
     {
@@ -453,6 +452,23 @@ public function verify2FA(Request $request)
             $currentUser->blockedUsers()->attach($userId);
             return response()->json(['isBlocked' => true]);
         }
+    }
+    public function getBlockedUsers(Request $request)
+    {
+        $user = Auth::user(); // Obtén el usuario autenticado
+
+        // Suponiendo que tienes una relación "blockedUsers" en el modelo User
+        // que devuelve una colección de instancias de UserBlock o similar
+        $blockedUsers = $user->blockedUsers()->with('blockedUser')->get()->map(function ($block) {
+            return [
+                'id' => $block->blockedUser->id,
+                'name' => $block->blockedUser->name,
+                'username' => $block->blockedUser->username,
+                // Puedes incluir más campos aquí según necesites
+            ];
+        });
+
+        return response()->json($blockedUsers);
     }
 
     public function destroy(Request $request): Response
