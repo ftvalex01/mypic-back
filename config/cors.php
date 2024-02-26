46e7bd78b1e28ae6b9fbcacff1b1cc8af45b7852
LucasVega2004 <luquitasvegablanco@gmail.com>
2024-02-26 03:59:11 +0000

    cors.php 2

--- config/cors.php
+++ config/cors.php
@@ -36,13 +36,13 @@ return [
     ],
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:3000')],
+    'allowed_origins' => ['https://lucasreact.informaticamajada.es'],
 
     'allowed_origins_patterns' => [],
 
     'allowed_headers' => ['*'],
 
-    'exposed_headers' => [],
+    'exposed_headers' => ['*'],
 
     'max_age' => 0,
 
